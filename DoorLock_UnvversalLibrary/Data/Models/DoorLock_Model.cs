ee82309185c51e557bb061b069bdce66fa406c7f
Neo Masemola <nggmasemola@outlook.com>
2019-10-28 20:58:42 +0200

    update Changes 2

--- DoorLock_UnvversalLibrary/Data/Models/DoorLock_Model.cs
+++ DoorLock_UnvversalLibrary/Data/Models/DoorLock_Model.cs
@@ -2,13 +2,16 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Newtonsoft.Json;
 
 namespace DoorLock_Library.Data.Models
 {
     public class DoorLock_Model
     {
+        [JsonProperty("iD")]
         public long _ID { get; set; }
 
+        [JsonProperty("doorState")]
         public bool _DoorState { get; set; }
     }
 }
